ecc4de5a0321b97ab768d75314ac161a82d62ba5
SOWMYAM112 <68852097+SOWMYAM112@users.noreply.github.com>
2020-11-21 19:22:32 +0530

    Add files via upload

---
+++ db_connect.php
@@ -0,0 +1,6 @@
+<?php 
+	$conn=mysqli_connect("localhost", "id15464034_root", "SOWmya11@#$$", "id15464034_banking");
+	if(!$conn) {
+		echo "Connection Error: " . mysqli_connect_error();
+	}
+?>
---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>$Banking System</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+  
+ <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+</head>
+
+<body>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+<a href="#"><img  src="2.jpg" style="height:50px;width:50px; margin-top:0px"></img></a>
+
+ <ul  >
+ <li style="text-align:right">
+                    <li style="text-align:center"><a href="viewUsers.php"style="text-decoration:none ;color:white" style="text-align:center">View Users</a></li>
+                   
+                    </ul>
+   
+</nav>
+<br>
+<BR>
+<BR>
+    <section>
+        <div class="container">
+        <div class="row">
+            <div class="col s6 description">
+            <strong>    <p><h5>Welcome to</h5>
+                <h3>i  Banking System</h3>
+                </p></strong>
+            </div>
+            <div class="col s6">
+                <img src="1.jpg" class="vector" width="300px" height="300px">
+            </div>
+        
+        <div class="go-to">
+        	<a href="transferHistory.php" class="waves-effect waves-light btn-large black z-depth-2">Transaction History</a>
+        	<a href="selectUser.php" class="waves-effect waves-light btn-large black z-depth-2">Select User</a>
+        </div>
+    </div>
+    
+    </section>
+    <br>
+    <br>
+    <br>
+    <footer class="footer fixed-bottom " style="background-color: black;">
+
+<div class="container">
+    <div class="row-justify content-center">
+        <div class="col-auto align-self-center">
+            <div  class="text-center"> 
+                <a class="btn btn-social-icon btn-envelope" href="mailto:sce17cs054@sairamtap.edu.in" style="color:blueviolet;"><i class="fa fa-envelope fa-lg"></i></a>
+                <a  class="btn btn-social-icon btn-github"href="https://github.com/SOWMYAM112" style="color:blueviolet;"><i class="fa fa-github"></i></a>
+                <a class="btn btn-social-icon btn-phone "href="tel:+918073543839" style="color:blueviolet;"><i class="fa fa-phone"></i></a>
+                <a class="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/sce17cs0054/" style="color:blueviolet;"><i class="fa fa-linkedin fa-lg"></i></a>
+
+            
+                <p  style="color:white">(c)SOWMYA TEDDY</p>
+            </div>
+        </div>
+    </div>
+</div>	
+
+</footer>
+
+
+</body>
+
+</html>
